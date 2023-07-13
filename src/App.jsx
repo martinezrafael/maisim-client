c3cb7749e0341e1c25a31b3e4e595fa870ccc927
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-13 17:36:15 -0300

    teste de cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../src/index.css';
-import Header from './components/global/Header';
-import UserList from './components/User/UserList';
+
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <UserList />
+      teste
     </div>
   )
 }
