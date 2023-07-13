3baa59366c347bf80fc53656a4c41a808fdc4b90
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-13 20:33:40 -0300

    adjust header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../src/index.css';
 import Top10List from './components/Comparativo2/Top10List';
-import Header from './components/global/Header';
+
 import CalcMetragem from './components/Comparativo1/CalcMetragem';
 
 
