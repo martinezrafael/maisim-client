426c2bdffb9a4776be4b666031f3e55d00ea8dc6
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-13 20:36:13 -0300

    ajust

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import '../src/index.css';
 import Top10List from './components/Comparativo2/Top10List';
 
-import CalcMetragem from './components/Comparativo1/CalcMetragem';
-
-
 const App = () => {
   return (
     <>
-      <CalcMetragem />
       <Top10List />
     </>
   )
