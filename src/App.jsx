08b37c998866c65ac4cc034bbead80bcdebd5e03
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-14 11:19:44 -0300

    ajust

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../src/index.css';
 import Top10List from './components/Comparativo2/Top10List';
+import UserList from './components/User/UserList';
 
 
 const App = () => {
   return (
     <>
       <Top10List />
+      <UserList />
     </>
   )
 }
