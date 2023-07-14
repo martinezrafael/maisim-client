2dee60917a569d2df415b0a20da8efc464982258
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-14 09:39:27 -0300

    test user from vercel with url maisim.universidadedafarmacia..com.br

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../src/index.css';
 import Top10List from './components/Comparativo2/Top10List';
+import UserList from './components/User/UserList';
 
 const App = () => {
   return (
     <>
       <Top10List />
+      <UserList />
     </>
   )
 }
--- src/components/Comparativo2/Top10List.jsx
+++ src/components/Comparativo2/Top10List.jsx
@@ -2,33 +2,35 @@ import React, { useState,useEffect } from "react";
 import axios from 'axios';
 
 
+
 const Top10List = () => {
   const [brickList, setBrickList] = useState([]);
 
   useEffect(() => {
     async function getBrick() {
       try {
-        const response = await axios.get('https://apiudf.azurewebsites.net/top/100/GENERICO');
+        const response = await axios.get('https://apiudf.azurewebsites.net/top/4');
         setBrickList(response.data);
       } catch (error) {
         console.error(error);
       }
     }
     getBrick();
+    
   },[])
 
   return (
     <div>
-      <table>
+      <table >
         <thead>
           <th>Nome</th>
           <th>Laboratório</th>
           <th>Share</th>
         </thead>
-        <tbody>
+        <tbody className="brickTable">
           {
             brickList.map((props, index) =>(
-              <tr key={index}>
+              <tr key={index} className={`item-${index+=1}`}>
                 <td>{props.PRODUTO}</td>
                 <td>{props.LABORATORIO}</td>
                 <td>{props.UNIDADES}</td>
