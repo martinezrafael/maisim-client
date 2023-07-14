934a748ee66e4489c4a91f4b53c145b98b8e7851
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-14 10:57:43 -0300

    ajust

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../src/index.css';
+import Header from '../src/components/Global/Header';
 import UserList from './components/User/UserList';
 
 const App = () => {
   return (
     <>
+      <Header />
       <UserList />
     </>
   )
