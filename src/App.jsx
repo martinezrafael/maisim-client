3a7c4e83a650ebb101b92e27d6798a5ada5c8663
rafamartinez <rafael.martinez@rmfarma.com.br>
2023-07-14 09:42:10 -0300

    test req user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../src/index.css';
-import Top10List from './components/Comparativo2/Top10List';
 import UserList from './components/User/UserList';
 
 const App = () => {
   return (
     <>
-      <Top10List />
       <UserList />
     </>
   )
